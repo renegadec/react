731e2525288ba1e8cbe8c5cebcc752fcff7ed5c0
africoiner <confidencenyirenda7@gmail.com>
2022-07-01 22:14:56 +0200

    inital commit

---
+++ practise/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+export default function App() {
+    return (
+        <div className="div-container">
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
---
+++ practise/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <i className="fa-brands fa-twitter-square"></i>
+            <i className="fa-brands fa-facebook-square"></i>
+            <i className="fa-brands fa-instagram-square"></i>
+            <i className="fa-brands fa-github-square"></i>
+        </div>
+    )
+}
