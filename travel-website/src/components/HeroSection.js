63678496fa2c05a6ef56ab944d1f68d19f7df07f
africoiner <confidencenyirenda7@gmail.com>
2022-08-06 18:52:26 +0200

    final project but still has bugs to be fixed

--- travel-website/src/components/HeroSection.js
+++ travel-website/src/components/HeroSection.js
@@ -5,7 +5,6 @@ import './HeroSection.css'
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src='/videos/video-2.mp4' autoPlay loop muted />
         <h1>Adventure Awaits</h1>
         <p>What are you waiting for?</p>
         <div className="hero-btns">
