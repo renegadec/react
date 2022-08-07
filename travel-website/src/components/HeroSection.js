0fc230d8fd874661acb84817e65855cd7a9ac69c
africoiner <confidencenyirenda7@gmail.com>
2022-08-07 15:57:50 +0200

    bug fix

--- travel-website/src/components/HeroSection.js
+++ travel-website/src/components/HeroSection.js
@@ -8,10 +8,18 @@ function HeroSection() {
         <h1>Adventure Awaits</h1>
         <p>What are you waiting for?</p>
         <div className="hero-btns">
-            <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+            <Button 
+              className='btns' 
+              buttonStyle='btn--outline' 
+              buttonSize='btn--large'
+            >
                 GET STARTED
             </Button>
-            <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
+            <Button 
+              className='btns' 
+              buttonStyle='btn--primary' 
+              buttonSize='btn--large'
+            >
                 WATCH TRAILER <i className="far fa-play-circle"></i>
             </Button>
         </div>
