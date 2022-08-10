40658b849b871dfb68cf6c11bb32d2c69b794872
africoiner <confidencenyirenda7@gmail.com>
2022-08-10 22:22:30 +0200

    added about me section and added styling

--- portfolio/src/components/experience/Experience.jsx
+++ portfolio/src/components/experience/Experience.jsx
@@ -3,7 +3,22 @@ import './Experience.css'
 
 const Experience = () => {
     return (
-        <div>Experience</div>
+        <section id='experience'>
+            <h5>Skills I have</h5>
+            <h2>My Experience</h2>
+
+            <div className="container experience__container">
+                <div className="experience__frontend">
+                    <h3>Frontend Development</h3>
+                    <div className="experience__content">
+                        
+                    </div>
+                </div>
+                <div className="experience_backend">
+
+                </div>
+            </div>
+        </section>
     )
 }
 
--- portfolio/src/components/nav/Navbar.jsx
+++ portfolio/src/components/nav/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './Navbar.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { MdWorkOutline } from 'react-icons/md'
+import { RiServiceLine } from 'react-icons/ri'
+import { RiMessage2Line } from 'react-icons/ri'
 
 const Navbar = () => {
+
+    const [activeNav, setActiveNav] = React.useState('#')
+
     return (
-        <div>Navbar</div>
+        <nav>
+            <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+            <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+            <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdWorkOutline /></a>
+            <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiMessage2Line /></a>
+        </nav>
     )
 }
 
