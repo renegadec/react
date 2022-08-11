a1d58ff34e670e4df699fea6cc9776e9ae553cba
africoiner <confidencenyirenda7@gmail.com>
2022-08-11 18:58:23 +0200

    added testimonials and portfolio components and styling

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -1,9 +1,47 @@
-// import React from 'react'
+import React from 'react'
 import './Contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsTelegram} from 'react-icons/bs'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
     return (
-        <section id='contact'>Contact</section>
+        <section id='contact'>
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className="contact__option">
+                        <MdOutlineEmail className='contact__option_icon' />
+                        <h4>Email</h4>
+                        <h5>work@africoiner.com</h5>
+                        <a href="mailto:work@africoiner.com" target="_blank" rel="noreferrer">Send a message</a>
+                    </article>
+                    <article className="contact__option">
+                        <BsTelegram className='contact__option_icon' />
+                        <h4>Telegram</h4>
+                        <h5>AfriCoiner</h5>
+                        <a href="https://t.me/africoiner" target="_blank" rel="noreferrer">Send a message</a>
+                    </article>
+                    <article className="contact__option">
+                        <BsWhatsapp className='contact__option_icon' />
+                        <h4>WhatsApp</h4>
+                        <h5>AfriCoiner</h5>
+                        <a href="https://api.whatsapp.com/send?phone=263783726458" target="_blank" rel="noreferrer">Send a message</a>
+                    </article>
+                </div>
+
+                {/* END OF CONTACT OPTIONS */}
+
+                <form action="">
+                    <input type="text" name='name' placeholder='Your Full Name' required />
+                    <input type="email" name='email' placeholder='Your Email' required />
+                    <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+                    <button type='submit' className='btn btn-primary'>Send Message</button>
+                </form>
+            </div>
+        </section>
     )
 }
 
--- portfolio/src/components/portfolio/Portfolio.jsx
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: 'Travla Website - Traveling Agency Site',
+        github:'https://github.com/renegadec/react/tree/main/travel-website',
+        demo:'https://trvala.netlify.app/'
+    },
+    {
+        id: 2,
+        image: IMG2,
+        title: 'Cryptocurrency Dashboard and Live Price Tracker',
+        github:'https://github.com/renegadec/react/tree/main/travel-website',
+        demo:'https://trvala.netlify.app/'
+    },
+    {
+        id: 3,
+        image: IMG3,
+        title: 'Bitcoin Web Wallet - Store your bitcoins',
+        github:'https://github.com/renegadec/react/tree/main/travel-website',
+        demo:'https://trvala.netlify.app/'
+    },
+    {
+        id: 4,
+        image: IMG4,
+        title: 'Cake Shop E-commerce Website',
+        github:'https://github.com/renegadec/react/tree/main/travel-website',
+        demo:'https://trvala.netlify.app/'
+    },
+    {
+        id: 5,
+        image: IMG5,
+        title: 'School Website - Website Student application',
+        github:'https://github.com/renegadec/react/tree/main/travel-website',
+        demo:'https://trvala.netlify.app/'
+    },
+    {
+        id: 5,
+        image: IMG6,
+        title: 'Hip Hop Artist Website - For booking artist',
+        github:'https://github.com/renegadec/react/tree/main/travel-website',
+        demo:'https://trvala.netlify.app/'
+    },
+]
 
 const Portfolio = () => {
     return (
-        <section id='portfolio'>Portfolio</section>
+        <section id='portfolio'>
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+
+            <div className="container portfolio__container">
+                {
+                    data.map(({id, image, title, github, demo}) => {
+                        return (
+                            <article key={id} className='portfolio__item'>
+                                <div className="portfolio__item_image">
+                                    <img src={image} alt={title} />
+                                </div>
+                                <h3>{title}</h3>
+                                <div className="portfolio__item_cta">
+                                    <a href={github} className='btn'>Github</a>
+                                    <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+            </div>
+        </section>
     )
 }
 
