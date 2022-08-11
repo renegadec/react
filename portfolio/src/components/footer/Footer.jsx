d9461b794d0d12cdd39a27214055777624733b46
africoiner <confidencenyirenda7@gmail.com>
2022-08-11 22:09:56 +0200

    added footer and changed floating navbar

--- portfolio/src/components/footer/Footer.jsx
+++ portfolio/src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './Footer.css'
+import {BsFacebook, BsInstagram, BsLinkedin} from 'react-icons/bs'
+import {SiTwitter} from 'react-icons/si'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <a href="#" className='footer__logo'>AfriCoiner</a>
+
+            <ul className='permalinks'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#testimonials">Testimonials</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className="footer__socials">
+                <a href="https://facebook.com"><BsFacebook /></a>
+                <a href="https://instagram.com"><BsInstagram /></a>
+                <a href="https://linkedin.com"><BsLinkedin /></a>
+                <a href="https://twitter.com"><SiTwitter /></a>
+            </div>
+
+            <div className="footer__copyright">
+                <small>&copy; AfriCoiner. All rights reserved</small>
+            </div>
+        </footer>
     )
 }
 
